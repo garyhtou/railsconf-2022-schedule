80762386853397c299e795fc56497cd107b50ffb
Gary Tou <gary@garytou.com>
2022-05-15 22:03:35 -0700

    Patch duplicate events

--- src/helpers/calendar.ts
+++ src/helpers/calendar.ts
@@ -1,13 +1,14 @@
 import { getEvents } from './events';
 import ical from 'ical-generator';
 
-const calendar = ical({
-	name: 'RailsConf 2022',
-	description: 'Schedule for RailsConf 2022 in Portland, OR. May 16th - 19th.',
-	timezone: 'America/Los_Angeles',
-});
-
 async function getCalendar() {
+	const calendar = ical({
+		name: 'RailsConf 2022',
+		description:
+			'Schedule for RailsConf 2022 in Portland, OR. May 16th - 19th.',
+		timezone: 'America/Los_Angeles',
+	});
+
 	const events = await getEvents();
 
 	// add events to calendar
